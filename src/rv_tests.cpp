b3ad828861d536727fd4e52b2eb737b40cccbe0b
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2024-11-05 15:44:19 +0530

    arm64 development

--- src/rv_tests.cpp
+++ src/rv_tests.cpp
@@ -10,33 +10,37 @@ emulator emu;
 #define TICK_LIMIT 10000 // How many instructions to execute
 
 int main(int argc, char* argv[]) {
-  // initaite registers and memory
-  emu.init(argv[1]);
-	printf("Running riscv-test image at %s \t result: ", argv[1]);
 
-  for (uint32_t i = 0; i < TICK_LIMIT; i++) {
-    // steps through one architectural change of pc
-    emu.step();
+	for (int32_t test_no = 1; test_no < argc; test_no++) {
+		// initiate memory
+		emu.init(argv[test_no]);
+		printf("Running riscv-test image: %s \t result: ", argv[test_no]);
+		for (uint32_t i = 0; i < TICK_LIMIT; i++) {
+			// steps through one architectural change of pc
+			emu.step();
 
-    // sets up interrupts to move to exception handler
-    // in next step()
-    emu.set_interrupts();
+			// sets up interrupts to move to exception handler
+			// in next step()
+			emu.set_interrupts();
 
-		uint64_t ret = emu.memory_read(0x10001000);
-		switch (ret) {
-		case 0: // test not finished yet
-			break;
-		case 1: // test passed
-			printf("passed \n");
-			return 0;
-		
-		default: // test failed
-			printf("failed at test: %03ld \n", ret);
-			return 1;
-			break;
+			uint64_t ret = emu.memory_read(0x10001000);
+			switch (ret) {
+			case 0: // test not finished yet
+				break;
+			case 1: // test passed
+				printf("passed \n");
+				goto test_end;
+
+			default: // test failed
+				printf("failed at test: %03ld \n", ret);
+				return 1;
+				break;
+			};
 		};
-  }
-	// timeout
-	printf("failed due to timed out \n");
-  return 2;
+		// timeout
+		printf("failed due to timed out \n");
+		return 2;
+test_end:
+	};
+	return 0;
 }
